370b5fd7c40b8260cc3ee1a16b42f646a457fec1
com-sun <gnlslfflffl@gmail.com>
2022-06-05 23:00:07 +0900

    study: selection sort

---
+++ src/algorithm/sorting/selection/Main.java
@@ -0,0 +1,20 @@
+package algorithm.sorting.selection;
+
+import algorithm.sorting.bubble.Bubble;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1, 23, 346, 6, 2, 4234, 52346, 6275, 3, 5, 4462, 75, 24, 875, 7, 863};
+        for (int i : arr) {
+            System.out.println("i = " + i);
+        }
+
+        System.out.println("---------------------");
+
+        Selection.sort(arr);
+
+        for (int i : arr) {
+            System.out.println("i = " + i);
+        }
+    }
+}
--- src/algorithm/sorting/selection/Selection.java
+++ src/algorithm/sorting/selection/Selection.java
@@ -1,4 +1,26 @@
 package algorithm.sorting.selection;
 
 public class Selection {
+    public static void sort(int[] arr) {
+        sort(arr, arr.length);
+    }
+
+    private static void sort(int[] arr, int size) {
+        for (int i = 0; i < size; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < size; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            swap (i, minIndex, arr);
+        }
+    }
+
+    private static void swap(int i, int minIndex, int[] arr) {
+        int temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
 }
