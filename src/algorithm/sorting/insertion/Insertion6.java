48d184f3e2c14066c3bc3899ecc4a424a0a49bfb
com-sun <gnlslfflffl@gmail.com>
2022-06-19 13:16:34 +0900

    study: insertion sort

--- src/algorithm/sorting/insertion/Insertion6.java
+++ src/algorithm/sorting/insertion/Insertion6.java
@@ -1,4 +1,20 @@
 package algorithm.sorting.insertion;
 
 public class Insertion6 {
+
+    public static void sort(int[] arr) {
+
+        int size = arr.length;
+        for (int i = 1; i < size; i++) {
+            int target = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > target) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = target;
+        }
+    }
 }
--- src/algorithm/sorting/insertion/Main.java
+++ src/algorithm/sorting/insertion/Main.java
@@ -14,7 +14,7 @@ public class Main {
         System.out.println();
         System.out.println("---------------------");
 
-        Insertion5.sort(arr);
+        Insertion6.sort(arr);
 
         for (int i : arr) {
             System.out.print(i + " ");
