ab0c7f753e6ffb4135a3cd75dd23f30f48a7e18b
com-sun <gnlslfflffl@gmail.com>
2022-06-25 23:23:23 +0900

    study: sorting algorithm

---
+++ src/algorithm/sorting/shell/sortingNum2/Main.java
@@ -0,0 +1,31 @@
+package algorithm.sorting.shell.sortingNum2;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        List<Integer> list = new ArrayList();
+        for (int i = 0; i < n; i++) {
+            list.add(Integer.parseInt(br.readLine()));
+        }
+        Collections.sort(list);
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for (int i : list) {
+            bw.write(i + "\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
